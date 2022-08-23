71b1ee57c598d5b85704cb18ae8a835fe6962252
kyrylo <cyrillicw@gmail.com>
2022-08-23 13:17:39 +0200

    Add missed files

---
+++ src/internet/src/HttpsInternetResource.h
@@ -0,0 +1,128 @@
+/*
+*** Copyright 2021 ProximaX Limited. All rights reserved.
+*** Use of this source code is governed by the Apache 2.0
+*** license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include <memory>
+#include "boost/beast.hpp"
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/ssl.hpp>
+
+#include "supercontract/GlobalEnvironment.h"
+#include "supercontract/SingleThread.h"
+#include "internet/InternetResource.h"
+#include "OCSPVerifier.h"
+
+namespace sirius::contract::internet {
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
+namespace ssl = boost::asio::ssl;
+
+enum class RevocationVerificationMode {
+    SOFT, HARD
+};
+
+class HttpsInternetResource:
+        private SingleThread,
+        public InternetResource,
+        public std::enable_shared_from_this<HttpsInternetResource> {
+
+private:
+
+    enum class ConnectionState {
+        UNINITIALIZED, INITIALIZED, SHUTDOWN, CLOSED
+    };
+
+    GlobalEnvironment& m_environment;
+
+    std::string m_host;
+    std::string m_port;
+    std::string m_target;
+
+    tcp::resolver                           m_resolver;
+    beast::ssl_stream<beast::tcp_stream>    m_stream;
+    beast::flat_buffer                      m_buffer;
+    http::request<http::empty_body>         m_req;
+    http::parser<false, http::buffer_body>  m_res;
+    std::vector<uint8_t>                    m_readDataBuffer;
+
+    std::map<RequestId, std::unique_ptr<OCSPVerifier>> m_ocspVerifiers;
+    bool                                               m_handshakeReceived = false;
+
+    int m_connectionTimeout;
+    int m_ocspQueryTimerDelay;
+    int m_ocspQueryMaxEfforts;
+    std::optional<boost::asio::high_resolution_timer> m_timeoutTimer;
+
+    RevocationVerificationMode m_revocationVerificationMode;
+
+    ConnectionState m_state = ConnectionState::UNINITIALIZED;
+
+public:
+
+    HttpsInternetResource(
+            ssl::context& ctx,
+            GlobalEnvironment& globalEnvironment,
+            const std::string& host,
+            const std::string& port,
+            const std::string& target,
+            int bufferSize,
+            int connectionTimeout,
+            int ocspQueryTimerDelay,
+            int ocspQueryMaxEfforts,
+            RevocationVerificationMode mode );
+
+public:
+
+    void open( const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback ) override;
+
+    void read( const std::shared_ptr<AsyncCallback<std::optional<std::vector<uint8_t>>>>& callback ) override;
+
+    void close() override;
+
+    ~HttpsInternetResource() override;
+
+private:
+
+    void onHostResolved(
+            beast::error_code ec,
+            const tcp::resolver::results_type& results,
+            const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback );
+
+    void
+    onConnected( beast::error_code ec,
+                 const tcp::resolver::results_type::endpoint_type&,
+                 const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback );
+
+    void onHandshake( beast::error_code ec, const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback );
+
+    void onWritten( beast::error_code ec,
+                    std::size_t bytes_transferred,
+                    const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback );
+
+    void onRead(
+            beast::error_code ec,
+            std::size_t bytes_transferred,
+            const std::shared_ptr<AsyncCallback<std::optional<std::vector<uint8_t>>>>& callback );
+
+    void runTimeoutTimer();
+
+    void verifyOCSP( ssl::verify_context& c, const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback );
+
+    void onOCSPVerified( const RequestId& requestId, CertificateRevocationCheckStatus status, const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback );
+
+    void onExtendedHandshake( const std::shared_ptr<AsyncCallback<InternetResourceContainer>>& callback );
+
+    void onShutdown( beast::error_code ec );
+};
+
+}
---
+++ src/internet/src/OCSPHandler.h
@@ -0,0 +1,88 @@
+/*
+*** Copyright 2021 ProximaX Limited. All rights reserved.
+*** Use of this source code is governed by the Apache 2.0
+*** license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "log.h"
+#include "DebugInfo.h"
+#include "supercontract/GlobalEnvironment.h"
+#include "supercontract/SingleThread.h"
+
+#include <openssl/ocsp.h>
+
+namespace sirius::contract::internet {
+
+enum class CertificateRevocationCheckStatus {
+    VALID, REVOKED, UNDEFINED
+};
+
+class OCSPHandler: private SingleThread {
+
+private:
+
+    BIO*            m_socketBio = nullptr;
+    OCSP_REQ_CTX*   m_ctx = nullptr;
+    OCSP_RESPONSE*  m_response = nullptr;
+
+    // We do not own this object
+    OCSP_REQUEST*   m_request;
+    OCSP_CERTID*    m_requestId;
+    stack_st_X509*  m_chain;
+    X509_STORE*     m_store;
+
+    char*   m_host = nullptr;
+    char*   m_port = nullptr;
+    char*   m_path = nullptr;
+    int     m_ssl = 0;
+
+    GlobalEnvironment& m_globalEnvironment;
+
+    std::optional<boost::asio::high_resolution_timer> m_retryTimer;
+    const int m_timerDelayMs;
+    const int m_maxEfforts;
+    int m_effortsLeft;
+
+    // The callback might be both synchronous and asynchronous
+    // After calling the callback one can not know
+    // whether the object exists anymore
+    std::function<void( CertificateRevocationCheckStatus status )> m_callback;
+
+public:
+
+    OCSPHandler( std::string&& url,
+                 OCSP_REQUEST* request,
+                 OCSP_CERTID* requestId,
+                 stack_st_X509* chain,
+                 X509_STORE* store,
+                 GlobalEnvironment& globalEnvironment,
+                 std::function<void( CertificateRevocationCheckStatus status )> callback,
+                 int timerDelayMs,
+                 int maxEfforts );
+
+    OCSPHandler( const OCSPHandler& ) = delete;
+
+    OCSPHandler( OCSPHandler&& ) = delete;
+
+    OCSPHandler& operator=( OCSPHandler&& ) = delete;
+
+    OCSPHandler& operator=( const OCSPHandler& ) = delete;
+
+    void run();
+
+    ~OCSPHandler();
+
+private:
+
+    void sendRequest();
+
+    void connect();
+
+    void read();
+
+    void onResponseReceived();
+};
+
+}
