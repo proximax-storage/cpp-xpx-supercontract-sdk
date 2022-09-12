1411c02ac682933c46a3f92bf30e5554529da556
changyc9928 <changyc9928@gmail.com>
2022-09-12 18:06:53 +0800

    R21SC-77 Add more unit tests

--- src/internet/test/testHttpConnection.cpp
+++ src/internet/test/testHttpConnection.cpp
@@ -25,7 +25,7 @@ using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
 namespace sirius::contract::internet::test
 {
 
-#define TEST_NAME = HttpsConnection
+#define TEST_NAME = HttpConnection
 
     TEST(HttpConnection, ValidRead)
     {
@@ -99,7 +99,7 @@ namespace sirius::contract::internet::test
                                                  "</html>\n";
                     ASSERT_EQ( actual, expected ); },
                                                                                                  [] {}, globalEnvironment);
-                    connection->getContainer()->read(readCallback);
+                // connection->getContainer()->read(readCallback);
             },
             [] {},
             globalEnvironment);
@@ -191,40 +191,93 @@ namespace sirius::contract::internet::test
         ASSERT_FALSE(flag);
     }
 
-    // I'm not sure whether you want to verify the HTTP certificate cuz you did not set `m_stream.set_verify_callback` in ./src/internet/src/HttpInternetResource.cpp
-    // TEST(HttpConnection, RevokedCertificate)
-    // {
+    TEST(HttpConnection, NonExisting)
+    {
 
-    //     GlobalEnvironmentImpl globalEnvironment;
-    //     auto &threadManager = globalEnvironment.threadManager();
+        GlobalEnvironmentImpl globalEnvironment;
+        auto &threadManager = globalEnvironment.threadManager();
 
-    //     // ssl::context ctx{ssl::context::tlsv12_client};
-    //     // ctx.set_default_verify_paths();
-    //     // ctx.set_verify_mode(ssl::verify_peer);
+        // ssl::context ctx{ssl::context::tlsv12_client};
+        // ctx.set_default_verify_paths();
+        // ctx.set_verify_mode(ssl::verify_peer);
 
-    //     auto urlDescription = parseURL("http://revoked.badssl.com/");
+        auto urlDescription = parseURL("http://examples123.com");
 
-    //     ASSERT_TRUE(urlDescription);
-    //     ASSERT_FALSE(urlDescription->ssl);
-    //     ASSERT_EQ(urlDescription->port, "80");
+        ASSERT_TRUE(urlDescription);
+        ASSERT_FALSE(urlDescription->ssl);
+        ASSERT_EQ(urlDescription->port, "80");
 
-    //     auto connectionCallback = createAsyncQueryHandler<std::optional<InternetConnection>>(
-    //         [&](std::optional<InternetConnection> &&connection)
-    //         {
-    //             ASSERT_TRUE(!connection);
-    //         },
-    //         [] {}, globalEnvironment);
+        auto connectionCallback = createAsyncQueryHandler<std::optional<InternetConnection>>(
+            [&](std::optional<InternetConnection> &&connection)
+            {
+                ASSERT_FALSE(connection);
+            },
+            [] {}, globalEnvironment);
 
-    //     threadManager.execute([&]
-    //                           { InternetConnection::buildHttpInternetConnection(
-    //                                 globalEnvironment,
-    //                                 urlDescription->host,
-    //                                 urlDescription->port,
-    //                                 urlDescription->target,
-    //                                 16 * 1024,
-    //                                 30000,
-    //                                 connectionCallback); });
-    //     threadManager.stop();
-    // }
+        threadManager.execute([&]
+                              { InternetConnection::buildHttpInternetConnection(
+                                    globalEnvironment,
+                                    urlDescription->host,
+                                    urlDescription->port,
+                                    urlDescription->target,
+                                    16 * 1024,
+                                    30000,
+                                    connectionCallback); });
+        threadManager.stop();
+    }
+
+    TEST(HttpConnection, NonExistingTarget)
+    {
+
+        GlobalEnvironmentImpl globalEnvironment;
+        auto &threadManager = globalEnvironment.threadManager();
+
+        // ssl::context ctx{ssl::context::tlsv12_client};
+        // ctx.set_default_verify_paths();
+        // ctx.set_verify_mode(ssl::verify_peer);
+
+        auto urlDescription = parseURL("http://www.google.com/eg");
+
+        ASSERT_TRUE(urlDescription);
+        ASSERT_FALSE(urlDescription->ssl);
+        ASSERT_EQ(urlDescription->port, "80");
+
+        auto connectionCallback = createAsyncQueryHandler<std::optional<InternetConnection>>(
+            [&](std::optional<InternetConnection> &&connection)
+            {
+                ASSERT_TRUE(connection);
+                auto readCallback = createAsyncQueryHandler<std::optional<std::vector<uint8_t>>>([connection = std::move(*connection)](std::optional<std::vector<uint8_t>> &&res)
+                                                                                                 {
+                    ASSERT_TRUE( res.has_value() );
+                    std::string actual(res->begin(), res->end());
+                    const std::string expected = "<!DOCTYPE html>\n"
+                                                "<!-- saved from url=(0014)about:internet -->\n"
+                                                "<html lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"
+                                                "  <meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width\">\n"
+                                                "  <title>Error 404 (Not Found)!!1</title>\n"
+                                                "  <style>\n"
+                                                "    *{margin:0;padding:0}html,code{font:15px/22px arial,sans-serif}html{background:#fff;color:#222;padding:15px}body{margin:7% auto 0;max-width:390px;min-height:180px;padding:30px 0 15px}* > body{background:url(//www.google.com/images/errors/robot.png) 100% 5px no-repeat;padding-right:205px}p{margin:11px 0 22px;overflow:hidden}ins{color:#777;text-decoration:none}a img{border:0}@media screen and (max-width:772px){body{background:none;margin-top:0;max-width:none;padding-right:0}}#logo{background:url(//www.google.com/images/branding/googlelogo/1x/googlelogo_color_150x54dp.png) no-repeat;margin-left:-5px}@media only screen and (min-resolution:192dpi){#logo{background:url(//www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png) no-repeat 0% 0%/100% 100%;-moz-border-image:url(//www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png) 0}}@media only screen and (-webkit-min-device-pixel-ratio:2){#logo{background:url(//www.google.com/images/branding/googlelogo/2x/googlelogo_color_150x54dp.png) no-repeat;-webkit-background-size:100% 100%}}#logo{display:inline-block;height:54px;width:150px}\n"
+                                                "  </style>\n"
+                                                "  </head><body><a href=\"https://www.google.com/\"><span id=\"logo\" aria-label=\"Google\"></span></a>\n"
+                                                "  <p><b>404.</b> <ins>That's an error.</ins>\n"
+                                                "  </p><p>The requested URL <code>/signin</code> was not found on this server.  <ins>That's all we know.</ins>\n"
+                                                "</p></body></html>";
+                    ASSERT_EQ( actual, expected ); },
+                                                                                                 [] {}, globalEnvironment);
+                connection->getContainer()->read(readCallback);
+            },
+            [] {}, globalEnvironment);
+
+        threadManager.execute([&]
+                              { InternetConnection::buildHttpInternetConnection(
+                                    globalEnvironment,
+                                    urlDescription->host,
+                                    urlDescription->port,
+                                    urlDescription->target,
+                                    16 * 1024,
+                                    30000,
+                                    connectionCallback); });
+        threadManager.stop();
+    }
 
 }
