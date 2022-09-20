35b55bc595444311937e9efc9ffed28e3d500645
changyc9928 <changyc9928@gmail.com>
2022-09-20 17:47:43 +0800

    R21SC-77 Implement simple unit test for the VM

--- libs/virtualMachine/test/testVirtualMachine.cpp
+++ libs/virtualMachine/test/testVirtualMachine.cpp
@@ -21,15 +21,34 @@ TEST(VirtualMachine, Example) {
 
     threadManager.execute([&] {
         // TODO Fill in the address
-        std::string address;
+        std::string address = "127.0.0.1:50051";
         RPCVirtualMachineBuilder builder;
-        pVirtualMachine = builder.build(storageObserver, environment, address);
+        auto pVirtualMachine = builder.build(storageObserver, environment, address);
 
         // TODO fill in the callRequest fields
-        CallRequest callRequest;
+        std::vector<uint8_t> params;
+        CallRequest callRequest = {
+            ContractKey(),
+            CallId(),
+            "../../../rust-xpx-supercontract-client-sdk/pkg/simple_contract.wasm",
+            "run",
+            params,
+            52000000,
+            20 * 1024,
+            CallRequest::CallLevel::AUTOMATIC,
+            CallReferenceInfo {
+                {},
+                0,
+                BlockHash(),
+                0,
+                0,
+                {}
+}
+        };
 
         auto[_, callback] = createAsyncQuery<std::optional<CallExecutionResult>>([] (auto&& res) {
             // TODO on call executed
+            ASSERT_TRUE(res);
         }, [] {}, environment, false, false);
 
         pVirtualMachine->executeCall(callRequest, std::weak_ptr<VirtualMachineInternetQueryHandler>(),
