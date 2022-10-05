141a17d6668dcdedb11a7d959dd5b318cca4fdfa
changyc9928 <changyc9928@gmail.com>
2022-10-05 20:15:38 +0800

    Add more unit tests

--- libs/virtualMachine/test/testVirtualMachine.cpp
+++ libs/virtualMachine/test/testVirtualMachine.cpp
@@ -644,10 +644,10 @@ namespace sirius::contract::vm::test
             // std::cout << res->m_return << std::endl;
             // std::cout << res->m_scConsumed << std::endl;
             // std::cout << res->m_smConsumed << std::endl;
-//            ASSERT_EQ(res->m_success, true);
-//            ASSERT_EQ(res->m_return, 1);
-//            ASSERT_EQ(res->m_scConsumed, 15183918183278);
-//            ASSERT_EQ(res->m_smConsumed, 10240);
+           ASSERT_EQ(res->m_success, false);
+           ASSERT_EQ(res->m_return, 0);
+           ASSERT_EQ(res->m_scConsumed, 0);
+           ASSERT_EQ(res->m_smConsumed, 0);
         }, [] {}, environment, false, false);
 
         pVirtualMachine->executeCall(callRequest, internetHandler,
@@ -663,5 +663,161 @@ namespace sirius::contract::vm::test
         threadManager.stop();
         exec("cp ../../libs/virtualMachine/test/supercontracts/lib.rs ../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/src/lib.rs");
     }
+
+    TEST(VirtualMachine, FaultyContract)
+    {
+
+        GlobalEnvironmentMock environment;
+        auto &threadManager = environment.threadManager();
+
+        auto storageObserver = std::make_shared<StorageContentManagerMock>();
+
+        std::shared_ptr<VirtualMachine> pVirtualMachine;
+
+        std::shared_ptr<MockVirtualMachineInternetQueryHandler> internetHandler;
+
+        std::promise<void> p;
+        auto barrier = p.get_future();
+
+        threadManager.execute([&]
+                              {
+        // The contract should panic in this case due to failing the assertion
+        exec("cp ../../libs/virtualMachine/test/supercontracts/internet_read_faulty.rs ../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/src/lib.rs");
+        exec("wasm-pack build --debug ../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/");
+        // TODO Fill in the address
+        std::string address = "127.0.0.1:50051";
+        RPCVirtualMachineBuilder builder;
+        pVirtualMachine = builder.build(storageObserver, environment, address);
+
+        // TODO fill in the callRequest fields
+        std::vector<uint8_t> params;
+        CallRequest callRequest = {
+            ContractKey(),
+            CallId(),
+            "../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/pkg/sdk_bg.wasm",
+            "run",
+            params,
+            25000000000,
+            // It needs to be 16kb more than the actual amount needed to pass the memory read limit check
+            // (the last call to read rpc function should expect 0 return but it will need to pass the 16kb check first anyways)
+            26 * 1024,
+            CallRequest::CallLevel::MANUAL,
+            CallReferenceInfo {
+                {},
+                0,
+                BlockHash(),
+                0,
+                0,
+                {}
+            }
+        };
+
+        internetHandler = std::make_shared<MockVirtualMachineInternetQueryHandler>();
+
+        auto[_, callback] = createAsyncQuery<CallExecutionResult>([&] (auto&& res) {
+            // TODO on call executed
+            p.set_value();
+            ASSERT_TRUE(res);
+            // std::cout << res->m_success << std::endl;
+            // std::cout << res->m_return << std::endl;
+            // std::cout << res->m_scConsumed << std::endl;
+            // std::cout << res->m_smConsumed << std::endl;
+            ASSERT_EQ(res->m_success, false);
+            ASSERT_EQ(res->m_return, 0);
+            ASSERT_EQ(res->m_scConsumed, 20525636378);
+            ASSERT_EQ(res->m_smConsumed, 10240);
+        }, [] {}, environment, false, false);
+
+        pVirtualMachine->executeCall(callRequest, internetHandler,
+                                     std::weak_ptr<VirtualMachineBlockchainQueryHandler>(), callback); });
+
+        barrier.get();
+
+        threadManager.execute([&]
+                              { pVirtualMachine.reset(); });
+
+        threadManager.stop();
+        exec("cp ../../libs/virtualMachine/test/supercontracts/lib.rs ../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/src/lib.rs");
+    }
+
+    // TEST(VirtualMachine, BlockConnection)
+    // {
+
+    //     GlobalEnvironmentMock environment;
+    //     auto &threadManager = environment.threadManager();
+
+    //     auto storageObserver = std::make_shared<StorageContentManagerMock>();
+
+    //     std::shared_ptr<VirtualMachine> pVirtualMachine;
+
+    //     std::shared_ptr<MockVirtualMachineInternetQueryHandler> internetHandler;
+
+    //     std::promise<void> p;
+    //     auto barrier = p.get_future();
+
+    //     threadManager.execute([&]
+    //                           {
+    //     exec("cp ../../libs/virtualMachine/test/supercontracts/long_run.rs ../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/src/lib.rs");
+    //     exec("wasm-pack build --debug ../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/");
+    //     // TODO Fill in the address
+    //     std::string address = "127.0.0.1:50051";
+    //     RPCVirtualMachineBuilder builder;
+    //     pVirtualMachine = builder.build(storageObserver, environment, address);
+
+    //     // TODO fill in the callRequest fields
+    //     std::vector<uint8_t> params;
+    //     CallRequest callRequest = {
+    //         ContractKey(),
+    //         CallId(),
+    //         "../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/pkg/sdk_bg.wasm",
+    //         "run",
+    //         params,
+    //         20000000000000,
+    //         // It needs to be 16kb more than the actual amount needed to pass the memory read limit check
+    //         // (the last call to read rpc function should expect 0 return but it will need to pass the 16kb check first anyways)
+    //         26 * 1024,
+    //         CallRequest::CallLevel::MANUAL,
+    //         CallReferenceInfo {
+    //             {},
+    //             0,
+    //             BlockHash(),
+    //             0,
+    //             0,
+    //             {}
+    //         }
+    //     };
+
+    //     internetHandler = std::make_shared<MockVirtualMachineInternetQueryHandler>();
+
+    //     auto[_, callback] = createAsyncQuery<CallExecutionResult>([&] (auto&& res) {
+    //         // TODO on call executed
+    //         p.set_value();
+    //         ASSERT_TRUE(res);
+    //         // std::cout << res->m_success << std::endl;
+    //         // std::cout << res->m_return << std::endl;
+    //         // std::cout << res->m_scConsumed << std::endl;
+    //         // std::cout << res->m_smConsumed << std::endl;
+    //         ASSERT_EQ(res->m_success, false);
+    //         ASSERT_EQ(res->m_return, 0);
+    //         ASSERT_EQ(res->m_scConsumed, 20525636378);
+    //         ASSERT_EQ(res->m_smConsumed, 10240);
+    //     }, [] {}, environment, false, false);
+
+    //     pVirtualMachine->executeCall(callRequest, internetHandler,
+    //                                  std::weak_ptr<VirtualMachineBlockchainQueryHandler>(), callback); });
+
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    //     threadManager.execute([&]
+    //                           { exec("sudo iptables -A INPUT -p tcp --dport 50051 -s 127.0.0.1 -j DROP"); });
+
+    //     barrier.get();
+
+    //     threadManager.execute([&]
+    //                           { pVirtualMachine.reset(); });
+
+    //     threadManager.stop();
+    //     exec("sudo iptables -D INPUT 1");
+    //     exec("cp ../../libs/virtualMachine/test/supercontracts/lib.rs ../../libs/virtualMachine/test/rust-xpx-supercontract-client-sdk/src/lib.rs");
+    // }
 
 }
