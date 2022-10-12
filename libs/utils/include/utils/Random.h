4b789f25e7230a9aeb8867bfac6b97b843bf6cec
kyrylo <cyrillicw@gmail.com>
2022-10-12 09:35:36 +0200

    Change Random

--- libs/utils/include/utils/Random.h
+++ libs/utils/include/utils/Random.h
@@ -12,11 +12,12 @@ namespace sirius::utils {
 
     template <class T>
     T generateRandomByteValue() {
-        T value;
+        std::array<uint8_t, sizeof(T)> data{};
         std::random_device rd;
         std::seed_seq sd{ rd(), rd(), rd(), rd() };
         std::mt19937 mt(sd);
-        std::generate_n(value.data(), sizeof(T), mt);
+        std::generate_n(data.data(), sizeof(data), mt);
+        T value(data);
         return value;
     }
 
