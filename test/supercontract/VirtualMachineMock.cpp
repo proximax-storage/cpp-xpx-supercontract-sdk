bb09574580a162895c20e8ebc056670f8c87d6ac
jackshen <chanjackshen@gmail.com>
2022-11-03 15:28:26 +0800

    Increase virtualMachine wait time

--- test/supercontract/VirtualMachineMock.cpp
+++ test/supercontract/VirtualMachineMock.cpp
@@ -17,8 +17,9 @@ void VirtualMachineMock::executeCall(const vm::CallRequest &request,
                                      std::weak_ptr<vm::VirtualMachineStorageQueryHandler> storageQueryHandler,
                                      std::shared_ptr<AsyncQueryCallback<vm::CallExecutionResult>> callback) {
     auto executionResult = m_result.front();
+    auto random = rand()%3000;
     m_result.pop_front();
-    m_timers[request.m_callId] = m_threadManager.startTimer(rand() % 1000, [=, this]() mutable {
+    m_timers[request.m_callId] = m_threadManager.startTimer(random, [=, this]() mutable {
         vm::CallExecutionResult result{
                 executionResult,
                 0,
