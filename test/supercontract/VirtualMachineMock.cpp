89a25e4753697877cfb94c25c18357ec1addcee8
jackshen <chanjackshen@gmail.com>
2022-11-10 15:05:23 +0800

    Modify virtualMachineMock for vm_unavailable test case

--- test/supercontract/VirtualMachineMock.cpp
+++ test/supercontract/VirtualMachineMock.cpp
@@ -21,23 +21,26 @@ void VirtualMachineMock::executeCall(const vm::CallRequest &request,
                                      std::weak_ptr<vm::VirtualMachineBlockchainQueryHandler> blockchainQueryHandler,
                                      std::weak_ptr<vm::VirtualMachineStorageQueryHandler> storageQueryHandler,
                                      std::shared_ptr<AsyncQueryCallback<vm::CallExecutionResult>> callback) {
+    if (m_virtualMachineIsFail == true && count < 4) {
+        count++;
+        m_timers[request.m_callId] = m_threadManager.startTimer(500, [=]() mutable {
+            callback->postReply(tl::unexpected<std::error_code>
+                    (vm::make_error_code(vm::VirtualMachineError::vm_unavailable)));
+        });
+        return;
+    }
+
     auto executionResult = m_result.front();
     auto random = rand()%3000;
     m_result.pop_front();
-    count++;
-    m_timers[request.m_callId] = m_threadManager.startTimer(random, [=, this]() mutable {
-        if(m_virtualMachineIsFail == true && count <= 3){
-            callback->postReply(tl::unexpected<std::error_code>
-                    (vm::make_error_code(vm::VirtualMachineError::vm_unavailable)));
-        }else{
-            vm::CallExecutionResult result{
-                    executionResult,
-                    0,
-                    0,
-                    0,
-            };
-            callback->postReply(result);
-        }
+    m_timers[request.m_callId] = m_threadManager.startTimer(random, [=]() mutable {
+        vm::CallExecutionResult result{
+                executionResult,
+                0,
+                0,
+                0,
+        };
+        callback->postReply(result);
     });
 }
 }
