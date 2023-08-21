f4e179eff48f429d337d0689f83c016cbc562d08
kyrylo <cyrillicw@gmail.com>
2023-08-21 23:44:59 +0200

    Ubuntu 22.04 fix

--- cmake/expected.cmake
+++ cmake/expected.cmake
@@ -1,7 +1,8 @@
+option(EXPECTED_BUILD_TESTS OFF)
 include(FetchContent)
 add_definitions(-w)
 FetchContent_Declare(
         expected
         GIT_REPOSITORY https://github.com/TartanLlama/expected
-        GIT_TAG        v1.0.0)
+        GIT_TAG        v1.1.0)
 FetchContent_MakeAvailable(expected)
